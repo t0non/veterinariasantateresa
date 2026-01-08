c7b8b2cbc73e591bd0122c2f7a987bd2d93b4e7d
Eduardo Tonon <eduardosoarestonon@gmail.com>
2026-01-08 23:14:38 +0000

    Isso ficou ótimo. As imagens estão com excelente qualidade e fundo trans

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { About } from '@/components/sections/about';
 import { Hero } from '@/components/sections/hero';
 import { Services } from '@/components/sections/services';
 import { SocialProof } from '@/components/sections/social-proof';
+import { Team } from '@/components/sections/team';
 import { WhoWeAre } from '@/components/sections/who-we-are';
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Services />
       <WhoWeAre />
+      <Team />
       <About />
       <SocialProof />
     </>
