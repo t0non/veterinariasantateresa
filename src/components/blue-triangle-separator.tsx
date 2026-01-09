6e4b26c208851523856762fe97309c2433b69a54
Eduardo Tonon <eduardosoarestonon@gmail.com>
2026-01-09 00:02:07 +0000

    olha como a cor ta diferente

--- src/components/blue-triangle-separator.tsx
+++ src/components/blue-triangle-separator.tsx
@@ -2,7 +2,7 @@
 export function BlueTriangleSeparator() {
   return (
     <div className="relative w-full z-10" style={{ marginBottom: '-1px' }}>
-      <div className="bg-primary bg-gradient-primary">
+      <div className="bg-gradient-primary">
           <div 
               className="w-full h-0 mx-auto"
               style={{
